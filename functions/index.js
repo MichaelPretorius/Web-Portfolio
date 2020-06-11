3a47b717752bd814019a2e4a5442bf68424c5c54
Michael Pretorius <michael.pretorius@younglings.africa>
2020-06-11 20:42:44 +0200

    removing captcha and changing email function to sendgrid

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,38 @@
 const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
-const rp = require("request-promise");
-const cors = require("cors")({ origin: true });
+const sgMail = require('@sendgrid/mail');
 
 const gmailEmail = functions.config().gmail.login;
-const gmailPassword = functions.config().gmail.pass;
+const sendgridAPI = functions.config().sendgrid.api;
+sgMail.setApiKey(sendgridAPI);
 
 // ==============================
 // FUNCTION TO SEND EMAIL
 // ==============================
 
 const goMail = (name, email, message) => {
+	const msg = {
+		to: gmailEmail,
+		from: gmailEmail,
+		subject: 'Web Profile Contact Request',
+		reply_to: email,
+		html: `<h2>You have a new contact request from your web portfolio</h2>
+    <h3>Name: </h3> <p>${name}</p>
+    <h3>Email: </h3> <p>${email}</p>
+    <h3>Message: </h3> <p>${message}</p>`,
+	};
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: gmailEmail,
-            pass: gmailPassword
-        }
-    });
-
-    const mailOptions = {
-        from: gmailEmail,
-        to: gmailEmail,
-        subject: 'Web Profile Contact Request',
-        html: `<h2>You have a new contact request from your web portfolio</h2>
-        <h3>Name: </h3> <p>${name}</p>
-        <h3>Email: </h3> <p>${email}</p>
-        <h3>Message: </h3> <p>${message}</p>`
-    };
-
-    const getDeliveryStatus = (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    };
-
-    transporter.sendMail(mailOptions, getDeliveryStatus);
+	return sgMail
+		.send(msg)
+		.then(() => {
+			console.log(
+				`Email sent successfully: name: ${name}, email: ${email}, message: ${message}`,
+			);
+		})
+		.catch(err => {
+			console.log(
+				`Email failed: error: ${err} name: ${name}, email: ${email}, message: ${message}`,
+			);
+		});
 };
 
 //onDataAdded watches for changes in database
@@ -50,41 +44,4 @@ exports.onDataAdded = functions.database.ref('/messages/{sessionId}').onCreate((
 
     goMail(name, email, message);
     return null;
-});
-
-
-// ==================================
-// FUNCTION FOR reCAPTCHA
-// ==================================
-
-const secretKey = functions.config().recaptcha.secret;
-
-exports.checkRecaptcha = functions.https.onRequest((req, res) => {
-
-    const captcha = req.body.captcha;
-    console.log("recaptcha response", captcha);
-
-    return cors(req, res, () => {
-        rp({
-            method: 'POST',
-            uri: 'https://recaptcha.google.com/recaptcha/api/siteverify',
-            formData: {
-                secret: secretKey,
-                response: captcha
-            },
-            json: true
-        }).then(result => {
-            console.log("Recaptcha Result: ", result);
-            if (result.success) {
-                res.json({ success: true, msg: "Captcha Passed." });
-            }
-            else {
-                res.json({ success: false, msg: "Captcha Verification Failed." });
-            }
-        }).catch(reason => {
-            console.log("Recaptcha Request Failure: ", reason);
-            res.json({ success: false, msg: "Error occured with Verification." });
-        })
-    });
-
 });
